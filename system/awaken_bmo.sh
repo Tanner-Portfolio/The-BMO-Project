94069cf7a5aed8cf132d602e83ad614e4ab0d8cb
Tanner-Portfolio <tazsmith5280@gmail.com>
2026-04-24 16:50:33 +0900

    Update awaken_bmo.sh

--- system/awaken_bmo.sh
+++ system/awaken_bmo.sh
@@ -5,6 +5,6 @@ amixer -c 0 sset Speaker 100% unmute
 
 # 2. Prepare Ollama (Loads BMO into RAM)
 # This is to prevent the first interaction from being slow
-curl -X POST http://localhost:####/api/generate -d '{"model": "bmo:latest", "keep_alive": -1}'
+curl -X POST http://localhost:11434/api/generate -d '{"model": "bmo:latest", "keep_alive": -1}'
 
 exit 0
